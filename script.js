ca8a290b39a394ff8e5940e7f3393629a8e8ba3e
keshab4855 <keshabroka123@gmail.com>
2022-05-21 04:24:43 +1000

    age and highest number

--- script.js
+++ script.js
@@ -20,25 +20,58 @@ Examples
 */
 //////////////SOLUTIONS//////////////////////////
 
-function sameCase(a, b) {
-  if (
-    a.toLowerCase() == a.toUpperCase() ||
-    b.toLowerCase() == b.toUpperCase()
-  ) {
-    return -1;
-  } else if (
-    (a.toLowerCase() == a && b.toLowerCase() == b) ||
-    (a.toUpperCase() == a && b.toUpperCase() == b)
-  ) {
-    return 1;
-  } else if (
-    (a.toLowerCase() !== a && b.toLowerCase() == b) ||
-    (a.toLowerCase() == a && b.toLowerCase() !== b) ||
-    (a.toUpperCase() !== a && b.toUpperCase() == b) ||
-    (a.toUpperCase() == a && b.toUpperCase() !== b)
-  ) {
-    return 0;
-  }
-}
+// function sameCase(a, b) {
+//   if (
+//     a.toLowerCase() == a.toUpperCase() ||
+//     b.toLowerCase() == b.toUpperCase()
+//   ) {
+//     return -1;
+//   } else if (
+//     (a.toLowerCase() == a && b.toLowerCase() == b) ||
+//     (a.toUpperCase() == a && b.toUpperCase() == b)
+//   ) {
+//     return 1;
+//   } else if (
+//     (a.toLowerCase() !== a && b.toLowerCase() == b) ||
+//     (a.toLowerCase() == a && b.toLowerCase() !== b) ||
+//     (a.toUpperCase() !== a && b.toUpperCase() == b) ||
+//     (a.toUpperCase() == a && b.toUpperCase() !== b)
+//   ) {
+//     return 0;
+//   }
+// }
 
 ///////////////////////////////////////////////////
+
+///////////////CHALLENGE 2///////////////////
+/*
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+*/
+
+///////////////////////////////////////////////////////////
+// twiceAsOld = (dadYearsOld, sonYearsOld) => {
+//   return Math.abs(dadYearsOld - 2 * sonYearsOld);
+// };
+// console.log(twiceAsOld(60, 50));
+///////////////////////////////////////////////////////////
+
+///////////////CHALLENGE 3///////////////////
+/*
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321
+*/
+
+// descendingOrder = (n) => {
+//   return parseInt(String(n).split("").sort().reverse().join(""));
+// };
+// console.log(descendingOrder(12483794));
