4bd389dfaff31b11dc81293e7a87b64f020d8f5b
keshab4855 <keshabroka123@gmail.com>
2022-05-22 23:03:52 +1000

    elevator challenge

--- script.js
+++ script.js
@@ -71,7 +71,39 @@ Input: 145263 Output: 654321
 Input: 123456789 Output: 987654321
 */
 
+///////////////////////////////////////////////////////////
+
 // descendingOrder = (n) => {
 //   return parseInt(String(n).split("").sort().reverse().join(""));
 // };
 // console.log(descendingOrder(12483794));
+///////////////////////////////////////////////////////////
+
+///////////////CHALLENGE 4///////////////////
+/*
+Given 2 elevators (named "left" and "right") in a building with 3 floors (numbered 0 to 2), write a function elevator accepting 3 arguments (in order):
+
+left - The current floor of the left elevator
+right - The current floor of the right elevator
+call - The floor that called an elevator
+It should return the name of the elevator closest to the called floor ("left"/"right").
+
+In the case where both elevators are equally distant from the called floor, choose the elevator to the right.
+
+You can assume that the inputs will always be valid integers between 0-2.
+
+Examples:
+
+elevator(0, 1, 0); // => "left"
+elevator(0, 1, 1); // => "right"
+elevator(0, 1, 2); // => "right"
+elevator(0, 0, 0); // => "right"
+elevator(0, 2, 1); // => "right"
+
+*/
+
+///////////////////////////////////////////////////////////
+const elevator = (left, right, call) =>
+  Math.abs(call - left) < Math.abs(call - right) ? "left" : "right";
+
+///////////////////////////////////////////////////////////
