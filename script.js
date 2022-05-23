f56254ab6f61bf4293b1d1eaea8a9c454883547b
keshab4855 <keshabroka123@gmail.com>
2022-05-24 02:51:37 +1000

    pillars solutions

--- script.js
+++ script.js
@@ -103,7 +103,28 @@ elevator(0, 2, 1); // => "right"
 */
 
 ///////////////////////////////////////////////////////////
-const elevator = (left, right, call) =>
-  Math.abs(call - left) < Math.abs(call - right) ? "left" : "right";
+// const elevator = (left, right, call) =>
+//   Math.abs(call - left) < Math.abs(call - right) ? "left" : "right";
 
+///////////////////////////////////////////////////////////
+
+///////////////CHALLENGE 5///////////////////
+
+/*
+There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+
+number of pillars (≥ 1);
+distance between pillars (10 - 30 meters);
+width of the pillar (10 - 50 centimeters).
+Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+
+ */
+
+///////////////////////////////////////////////////////////
+function pillars(num_pill, dist, width) {
+  // your code here
+  return num_pill > 1
+    ? (num_pill - 1) * dist * 100 + (num_pill - 2) * width
+    : 0;
+}
 ///////////////////////////////////////////////////////////
