13f9d55cc18618bc5c83c9b8117b06695d79117a
keshab4855 <keshabroka123@gmail.com>
2022-05-24 02:56:06 +1000

    array differences

--- script.js
+++ script.js
@@ -121,10 +121,24 @@ Calculate the distance between the first and the last pillar in centimeters (wit
  */
 
 ///////////////////////////////////////////////////////////
-function pillars(num_pill, dist, width) {
-  // your code here
-  return num_pill > 1
-    ? (num_pill - 1) * dist * 100 + (num_pill - 2) * width
-    : 0;
-}
+// function pillars(num_pill, dist, width) {
+//   // your code here
+//   return num_pill > 1
+//     ? (num_pill - 1) * dist * 100 + (num_pill - 2) * width
+//     : 0;
+// }
 ///////////////////////////////////////////////////////////
+
+///////////////CHALLENGE 6///////////////////
+
+/*
+Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+Example
+[2, 1, 10]  -->  9
+In descending order: [10, 2, 1]
+
+Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell ).
+*/
