2f9be3a65d21ba313e1ec7c8b742293dffc67205
keshab4855 <keshabroka123@gmail.com>
2022-05-30 05:17:21 +1000

    weekdays

--- script.js
+++ script.js
@@ -280,10 +280,54 @@ Return the type of the sum of the two arguments
 /*
 Create a table of 5 from 1 to 10
 */
-function multiTable(number) {
-  for (let i = 1; i < 11; i++) {
-    console.log(`${i} * ${number} = ${number * i} `);
+
+///////////////////////////////////////////////////////////
+
+// function multiTable(number) {
+//   for (let i = 1; i < 11; i++) {
+//     console.log(`${i} * ${number} = ${number * i} `);
+//   }
+//   return;
+// }
+// multiTable(5);
+
+///////////////////////////////////////////////////////////
+
+///////////////CHALLENGE 14///////////////////
+/*
+Complete the function which returns the weekday according to the input number:
+
+1 returns "Sunday"
+2 returns "Monday"
+3 returns "Tuesday"
+4 returns "Wednesday"
+5 returns "Thursday"
+6 returns "Friday"
+7 returns "Saturday"
+Otherwise returns "Wrong, please enter a number between 1 and 7"
+*/
+
+///////////////////////////////////////////////////////////
+
+function whatday(num) {
+  switch (num) {
+    case 1:
+      return "Sunday";
+    case 2:
+      return "Monday";
+    case 3:
+      return "Tuesday";
+    case 4:
+      return "Wednesday";
+    case 5:
+      return "Thursday";
+    case 6:
+      return "Friday";
+    case 7:
+      return "Saturday";
+    default:
+      return "Wrong, please enter a number between 1 and 7";
   }
-  return;
 }
-multiTable(5);
+whatday(8);
+///////////////////////////////////////////////////////////
