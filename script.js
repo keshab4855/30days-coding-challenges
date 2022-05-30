d821d58685533ba1c65fc5e5048e9d254d983e2f
keshab4855 <keshabroka123@gmail.com>
2022-05-31 03:07:42 +1000

    divisible number

--- script.js
+++ script.js
@@ -343,12 +343,39 @@ typeValidation("42", "number"); // => false
  */
 ///////////////////////////////////////////////////////////
 
-function typeValidation(variable, type) {
-  if (typeof variable === type) {
+// function typeValidation(variable, type) {
+//   if (typeof variable === type) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(typeValidation("9", "number"));
+///////////////////////////////////////////////////////////
+
+///////////////CHALLENGE 16///////////////////
+/*
+Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+
+A few cases:
+
+
+(-12, 2, -6)  ->  true
+(-12, 2, -5)  ->  false
+
+(45, 1, 6)    ->  false
+(45, 5, 15)   ->  true
+
+(4, 1, 4)     ->  true
+(15, -5, 3)   ->  true
+ */
+
+///////////////////////////////////////////////////////////
+function isDivideBy(number, a, b) {
+  if (number % a == 0 && number % b == 0) {
     return true;
   } else {
     return false;
   }
 }
-console.log(typeValidation("9", "number"));
 ///////////////////////////////////////////////////////////
