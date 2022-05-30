c12ae1e95e9a5a19d7e26e81d7e249c5b815b051
keshab4855 <keshabroka123@gmail.com>
2022-05-31 03:26:03 +1000

    replacing dot with dash

--- script.js
+++ script.js
@@ -388,12 +388,32 @@ Return True if yes, False otherwise :)
 */
 
 ///////////////////////////////////////////////////////////
-function hero(bullets, dragons) {
-  if (bullets >= 2 * dragons) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// function hero(bullets, dragons) {
+//   if (bullets >= 2 * dragons) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
+///////////////////////////////////////////////////////////
+
+///////////////CHALLENGE 18///////////////////
+
+/*
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+Notes
+String str will never be null.
+*/
+
+///////////////////////////////////////////////////////////
+var replaceDots = function (str) {
+  return str.replace(/\./g, "-");
+};
 ///////////////////////////////////////////////////////////
