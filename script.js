fe05a1fabfbab84574afb9bfb01ce5e3160b2843
keshab4855 <keshabroka123@gmail.com>
2022-05-31 03:16:02 +1000

    number of bullets to kill

--- script.js
+++ script.js
@@ -371,11 +371,29 @@ A few cases:
  */
 
 ///////////////////////////////////////////////////////////
-function isDivideBy(number, a, b) {
-  if (number % a == 0 && number % b == 0) {
+// function isDivideBy(number, a, b) {
+//   if (number % a == 0 && number % b == 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+///////////////////////////////////////////////////////////
+
+///////////////CHALLENGE 17///////////////////
+/*
+A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return True if yes, False otherwise :)
+*/
+
+///////////////////////////////////////////////////////////
+function hero(bullets, dragons) {
+  if (bullets >= 2 * dragons) {
     return true;
   } else {
     return false;
   }
 }
+
 ///////////////////////////////////////////////////////////
