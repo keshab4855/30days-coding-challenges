69e631e3a50ccb4ead13846a6a6cef589b7df55e
keshab4855 <keshabroka123@gmail.com>
2022-06-02 17:32:24 +1000

    sum of digits in binary number

--- script.js
+++ script.js
@@ -507,12 +507,26 @@ Input: 123456789 Output: 987654321
 
 ///////////////////////////////////////////////////////////
 
-function descendingOrder(n) {
-  let strNumber = n.toString();
-  let inputNumber = Array.from(strNumber, Number);
-  let arr = inputNumber.sort().reverse();
-  return Number(arr.join(""));
-  console.log();
-}
-descendingOrder(234);
+// function descendingOrder(n) {
+//   let strNumber = n.toString();
+//   let inputNumber = Array.from(strNumber, Number);
+//   let arr = inputNumber.sort().reverse();
+//   return Number(arr.join(""));
+//   console.log();
+// }
+// descendingOrder(234);
+///////////////////////////////////////////////////////////
+
+///////////////CHALLENGE 22///////////////////
+/*
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+*/
+///////////////////////////////////////////////////////////
+var countBits = function (n) {
+  let binNumber = Array.from(n.toString(2), Number);
+  return eval(binNumber);
+};
+countBits(1234);
 ///////////////////////////////////////////////////////////
